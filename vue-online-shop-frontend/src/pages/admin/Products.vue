392fb77944ebae9c2fe844c25fef35109b3d469e
qiaozhiyuan <18226851365@163.com>
2020-07-08 17:34:59 +0800

    vue shop

---
+++ vue-online-shop-frontend/src/pages/admin/Products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>This is Admin</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'products'
+}
+</script>
+
+<style>
+
+</style>
